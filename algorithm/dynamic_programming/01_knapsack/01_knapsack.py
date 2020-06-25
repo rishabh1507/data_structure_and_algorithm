67d88e39fc0df64817e8609fc2c8b09663366509
Rishabh Srivastava <srivastava829@gmail.com>
2020-06-25 21:19:21 +0530

    memoization added

--- algorithm/dynamic_programming/01_knapsack/01_knapsack.py
+++ algorithm/dynamic_programming/01_knapsack/01_knapsack.py
@@ -1,5 +1,6 @@
 #recursive
 
+"""
 def knapSack(W,wt,val,n):
     if(n==0 or W==0):
         return 0
@@ -15,3 +16,41 @@ n = len(val)
 
 result = knapSack(W,wt,val,n)
 print(result)
+
+"""
+
+# recursive + memoziation
+
+"""
+def knapSack(wt,val,dp,W, n):
+    if(n == 0 or W == 0):
+        return 0
+    if(dp[n][W] != -1):
+        return dp[n][W]
+    if(wt[n-1] <= W):
+        pick = val[n-1] + knapSack(wt,val,dp,W-wt[n-1],n-1)
+        drop = knapSack(wt,val,dp,W,n-1)
+        dp[n][W] = max(pick,drop)
+        return dp[n][W]
+    else:
+        dp[n][W] = knapSack(wt, val, dp, W, n-1)
+        return dp[n][W]
+
+
+
+
+
+val = [60, 100, 120]
+wt = [10, 20, 30]
+W = 50
+n = len(val)
+dp = [[-1 for x in range(n+1)] for x in range(W+1)] 
+print(dp)
+
+result = knapSack(wt, val, dp,W,n)
+print(result)
+
+"""
+
+#top-down approach
+
