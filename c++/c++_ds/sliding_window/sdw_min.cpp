c97d4a7952a91aa364403a34cf3c57e306a16a05
Rishabh Srivastava <srivastava829@gmail.com>
2020-10-21 21:58:00 +0530

    sdw

--- c++/c++_ds/sliding_window/sdw_min.cpp
+++ c++/c++_ds/sliding_window/sdw_min.cpp
@@ -1,6 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Problem: Given an array of positive numbers and a positive number ‘S’,
+// 	find the length of the smallest contiguous subarray whose sum is greater than
+// 	or equal to ‘S’. Return 0, if no such subarray exists.
+
+// Explanation when sum becomes greater we only check window size with curr size and we update min with curr
+
+// 	Sliding Window Pattern
+// {2, 1, 5, 2, 3, 2}
+
+// 	Output: 2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int smallsum(vector<int> arr, int w)
+{
+    int left = 0;
+    int right;
+    int res = 100000;
+    int sum1 = 0;
+    // loop and do check condition when >
+    for (right = 0; right < arr.size(); right++)
+    {
+        sum1 += arr[right];
+        while (sum1 >= w)
+        {
+            res = min(res, right - left + 1);
+            sum1 -= arr[left];
+            left++;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    vector<int> arr{2, 1, 5, 2, 3, 2};
+    int w = 7;
+    int result = smallsum(arr, w);
+    cout << result;
+    return 0;
+}
+
 // int minsubarrayK(vector<int> arr, int w)
 // {
 //     int left = 0;
